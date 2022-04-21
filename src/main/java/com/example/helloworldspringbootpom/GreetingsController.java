ed0b74ccdfb1a982878d086c70ecea846fa02dfa
VenkatgiriS <56349307+VenkatgiriS@users.noreply.github.com>
2022-04-21 23:47:14 +0530

    Update GreetingsController.java

--- src/main/java/com/example/helloworldspringbootpom/GreetingsController.java
+++ src/main/java/com/example/helloworldspringbootpom/GreetingsController.java
@@ -8,6 +8,6 @@ public class GreetingsController {
 
     @GetMapping("/")
     public String getGreeting() {
-        return "Hello!This is DevOps World";
+        return "Hello!This is Venkat's DevOps World";
     }
 }
