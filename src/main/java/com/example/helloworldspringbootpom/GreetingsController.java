6e50a6e2fb788c753494afb1db39e97ca49c0d18
VenkatgiriS <56349307+VenkatgiriS@users.noreply.github.com>
2022-04-24 11:12:35 +0530

    Update GreetingsController.java

--- src/main/java/com/example/helloworldspringbootpom/GreetingsController.java
+++ src/main/java/com/example/helloworldspringbootpom/GreetingsController.java
@@ -8,6 +8,6 @@ public class GreetingsController {
 
     @GetMapping("/")
     public String getGreeting() {
-        return "Hello!This is Venkat's DevOps World.Implemented using Ansible ,Docker and trying k8s deployment";
+        return "Hello!This is Venkat's DevOps World.Implemented using Ansible ,Docker and trying k8s now";
     }
 }
